b620ba5ae314cb365dedc2bbfcc1506546e6100e
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2023-08-23 16:31:20 -0300

    Refactor

--- js/script.js
+++ js/script.js
@@ -1,123 +1,186 @@
-window.app = {
-  input: '',
-  output: [],
-  directions: ['norte', 'sul', 'leste', 'oeste'],
-  objects: [
-    { tag: 'campo', desc: 'um campo', loc: '', weight: 9999 },
-    { tag: 'caverna', desc: 'uma caverna', loc: '', weight: 9999 },
-    { tag: 'leste', desc: 'uma entrada', loc: 'campo', weight: 9999, leadsTo: 'caverna' },
-    { tag: 'oeste', desc: 'uma saida', loc: 'caverna', weight: 9999, leadsTo: 'campo' },
-    { tag: 'guarda', desc: 'um guarda', loc: 'campo', weight: 80 },
-    { tag: 'adaga', desc: 'uma adaga', loc: 'guarda', weight: 2 },
-    { tag: 'prata', desc: 'uma moeda de prata', loc: 'campo', weight: 1 },
-    { tag: 'você', desc: 'você', loc: 'campo', weight: 80, maxWeight: 30 },
-  ],
-  processCommand(line) {
-    this.logToConsole('>' + line, 'para-cmd')
+class Room {
+  constructor(desc) {
+    this.desc = desc;
+    this.exits = {};
+    this.objects = [];
+  }
 
-    let toks = line.toLowerCase().trim().split(' ');
+  addExit(key, loc) {
+    this.exits[key] = loc;
+  }
+
+  addObject(obj) {
+    this.objects.push(obj);
+  }
+
+  removeObject(obj) {
+    this.objects.splice(this.objects.indexOf(obj), 1);
+  }
+
+  getObject(tag) {
+    return this.objects.filter((v) => v.tag === tag)[0];
+  }
+}
+
+class MyObject {
+  constructor(tag, desc, loc, weight) {
+    this.tag = tag;
+    this.desc = desc;
+    this.loc = loc;
+    this.weight = weight;
+  }
+
+  setLocation(newLoc) {
+    this.loc = newLoc;
+    this.loc.addObject(this);
+  }
+
+  move(newLoc) {
+    this.loc.removeObject(this);
+    this.setLocation(newLoc);
+  }
+}
+
+class Entity extends MyObject {
+  constructor(name, health, loc) {
+    super(name, name, loc, 80);
+
+    this.name = name;
+    this.health = health;
+    this.inventory = [];
+    this.actions = {};
+  }
+
+  addItem(item) {
+    this.inventory.push(item);
+  }
+
+  removeItem(item) {
+    this.inventory.splice(this.inventory.indexOf(item), 1);
+  }
+
+  addAction(key, func) {
+    this.actions[key] = func;
+  }
+
+  doAction(key, ...args) {
+    if (this.actions[key]) {
+      return this.actions[key](args);
+    } else {
+      return [false, [`${this.name} não pode ${key}.`]];
+    }
+  }
+}
+
+let rooms = [new Room("limbo"), new Room("um campo"), new Room("uma caverna")];
+
+let items = [
+  new MyObject("prata", "uma moeda de prata", rooms[1], 1),
+  new MyObject("adaga", "uma adaga", rooms[0], 2),
+];
+
+let entities = [
+  new Entity("guarda", 12, rooms[1]),
+  new Entity("você", 12, rooms[1]),
+];
+
+rooms[1].addExit("leste", rooms[2]);
+rooms[2].addExit("oeste", rooms[1]);
+
+entities[0].addItem(items[1]);
+
+items.forEach((item) => item.setLocation(item.loc));
+
+entities.forEach((e) => e.setLocation(e.loc));
+
+window.app = {
+  input: "",
+  output: [],
+  player: entities[1],
+  processCommand(line) {
+    this.logToConsole(">" + line, "para-cmd");
+
+    let toks = line.toLowerCase().trim().split(" ");
     let cmd = toks[0];
     let args = toks.slice(1, toks.length);
 
     let [code, result] = this.gameEval(cmd, args);
 
-    result.map(n => this.logToConsole(n, code ? 'para-res' : 'para-err'));
+    result.map((n) => this.logToConsole(n, code ? "para-res" : "para-err"));
 
-    this.input = '';
+    this.input = "";
   },
   gameEval(cmd, args) {
     switch (cmd) {
-      case 'olhar':
-        return this.look(args[0]);
-      case 'pegar':
+      case "olhar":
+        return this.look();
+      case "pegar":
         return this.pickup(args[0]);
-      case 'ir':
+      case "ir":
         return this.go(args[0]);
-      case 'norte':
-      case 'sul':
-      case 'leste':
-      case 'oeste':
+      case "norte":
+      case "sul":
+      case "leste":
+      case "oeste":
         return this.go(cmd);
       default:
-        return [false, [`Eu não sei "${cmd}".`]]
+        return [false, [`Eu não sei "${cmd}".`]];
     }
   },
-  look(where) {
-    let player = this.getObject('você');
-
-    if (!where || where === 'arredores') {
-      return [true, [`Você está em ${ this.getObject(player.loc).desc }.`].concat(this.descObjectsAt(player.loc).concat(this.descExitsAt(player.loc)))];
-    }
-
-    if (where === 'eu') where = 'você';
-
-    let target = this.getObject(where);
-    if (!target) {
-      return [false, ['Olhar para onde?']];
-    }
-
-    let descs = this.descObjectsAt(where);
-
-    return [true, descs.length ? this.descObjectsAt(where) : ['Não tem nada ai.']];
+  look() {
+    let loc = this.player.loc;
+    return [
+      true,
+      [`Você está em ${loc.desc}.`].concat(
+        loc.objects
+          .map((t) => `Tem ${t.desc} aqui.`)
+          .concat(
+            Object.keys(loc.exits).map(
+              (k) => `Tem ${loc.exits[k]} para o ${k} daqui.`
+            )
+          )
+      ),
+    ];
   },
   pickup(itemTag) {
     if (!itemTag) {
       return [false, [`Pegar o quê?`]];
     }
 
-    let player = this.getObject('você');
-    let item = this.getObject(itemTag);
+    let item = this.player.loc.getObject(itemTag);
 
-    if (!item || item.loc !== player.loc) {
+    if (!item) {
       return [false, [`Não há ${itemTag} aqui.`]];
     }
 
-    if (item.weight > player.maxWeight) {
-      return [false, [`${item.desc} é muito pesado para ${player.desc} carregar.`]];
+    if (item.weight > 30) {
+      return [
+        false,
+        [`${item.desc} é muito pesado para ${player.desc} carregar.`],
+      ];
     }
 
-    item.loc = player.tag;
+    item.move(rooms[0]);
+    this.player.addItem(item);
     return [true, [`Você pegou "${item.desc}".`]];
   },
   go(dir) {
     if (!dir) {
-      return [false, ['Ir para onde?']];
+      return [false, ["Ir para onde?"]];
     }
 
-    if (!this.directions.includes(dir)) {
-      return [false, [`${dir} não é uma direção.`]];
-    }
-
-    let player = this.getObject('você');
-    let exits = this.getExitsAt(player.loc);
-
-    let exit = exits.filter(v => v.tag === dir)[0];
+    let exit = this.player.loc.exits[dir];
 
     if (!exit) {
       return [false, [`Não tem nada a ${dir} daqui.`]];
     }
 
-    player.loc = exit.leadsTo;
+    this.player.move(exit);
     return this.look();
   },
   logToConsole(msg, cls) {
     this.output.push({ text: msg, class: cls });
   },
-  getObject(tag) {
-    return this.objects.filter(v => v.tag === tag && !this.directions.includes(v.tag))[0];
-  },
-  getObjectsAt(loc) {
-    return this.objects.filter(v => v.loc === loc && v.tag !== 'você' && !this.directions.includes(v.tag));
-  },
-  descObjectsAt(loc) {
-    return this.getObjectsAt(loc).map(v => `Tem ${v.desc} aqui.`);
-  },
-  getExitsAt(loc) {
-    return this.objects.filter(v => v.loc === loc).filter(v => v.leadsTo && this.directions.includes(v.tag));
-  },
-  descExitsAt(loc) {
-    return this.getExitsAt(loc).map(v => `Tem ${v.desc} para o ${v.tag} daqui.`);
-  }
 };
 
 PetiteVue.createApp(app).mount();
