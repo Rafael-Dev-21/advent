8e89c7b78f95a35da8908629b31bf62ed33acb64
Victor Rafael Pereira Alves <victorrafaelpereiraalves0202@gmail.com>
2023-08-27 22:12:32 -0300

    agora provavelmente funciona

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
   let config = {
+    base: "./",
     plugins: [vue()],
     resolve: {
       alias: {
